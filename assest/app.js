61cb01ec28593e8f9ef5b41ce48b5736fd2c76f9
unknown <junaidxhaikh789@gmail.com>
2023-09-02 11:25:43 +0500

    junaid-portfolio

---
+++ assest/app.js
@@ -0,0 +1,46 @@
+// Page loading animation
+$(window).on('load', function () {
+    if ($('.cover').length) {
+        $('.cover').parallax({
+            imageSrc: $('.cover').data('image'),
+            zIndex: '1'
+        });
+    }
+
+    $("#preloader").animate({
+        'opacity': '0'
+    }, 600, function () {
+        setTimeout(function () {
+            $("#preloader").css("visibility", "hidden").fadeOut();
+        }, 300);
+    });
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var box = $('.header-text').height();
+    var header = $('header').height();
+
+    if (scroll >= box - header) {
+        $("header").addClass("background-header");
+    } else {
+        $("header").removeClass("background-header");
+    }
+});
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+//*****************************
+    // Match Height Functions
+    //*****************************
+    $('.matchheight').matchHeight();
